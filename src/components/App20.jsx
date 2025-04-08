c7b22ac01509d7b20c8edad67f4005e4dabf6385
anusha0225 <anusha02049@gmail.com>
2025-04-08 09:58:59 +0530

    added

---
+++ src/components/App20.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class App20 extends Component {
+  render() {
+    return (
+      <div style={{textAlign: "center"}}>
+        <h1>This is App20</h1>
+        <p>It is a class component</p>
+      </div>
+    )
+  }
+}
---
+++ src/components/App21.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class App21 extends Component {
+  render() {
+    return (
+      <div style={{textAlign: "center"}}>
+        <h1>This is App21</h1>
+        
+      </div>
+    )
+  }
+}
